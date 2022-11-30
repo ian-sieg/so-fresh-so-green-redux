c0c5951a945e73067f049179caec75670201eda0
ian.moss <ianmsieg@gmail.com>
2022-11-30 10:01:08 -0600

    added basic todo redux list with add and remove feature

---
+++ src/features/todos/Todo.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+    addTodo,
+    removeTodo,
+    selectTodos
+} from './todoSlice'
+
+export function Todo() {
+    const todos = useSelector(selectTodos)
+    const dispatch = useDispatch()
+    const [todo, setTodo] = useState('')
+
+    return (
+        <div>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={todo.key} style={{listStyle: 'none'}}><button onClick={() => (dispatch(removeTodo(todo.id)))}>delete</button> {todo.text}</li>
+                ))}
+            </ul>
+            <input
+                type='text'
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+            />
+            <button
+                onClick={() => {
+                    dispatch(addTodo(todo));
+                    setTodo('')
+                }}
+            >Add Todo</button>
+        </div>
+    )
+}
